d2b9c12d23d4a7333cfaf4190f2b85279fbd5a6d
Nazar Chuiko <rondelman15@gmail.com>
2021-10-22 13:39:56 +0300

    inc

--- lab_3/app.py
+++ lab_3/app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+
 @app.route("/blog")
 def blog():
     news_dict = {
@@ -12,7 +13,10 @@ def blog():
         'Third Day': 'I happy',
 
     }
-    return render_template("blog.html", news_dict=news_dict, sys_info=request.headers.get('User-Agent'), sys=sys.version, os_name=os.name, platform=platform.system(), release=platform.release(), date=datetime.now())
+    return render_template("blog.html", news_dict=news_dict, sys_info=request.headers.get('User-Agent'),
+                           sys=sys.version, os_name=os.name, platform=platform.system(), release=platform.release(),
+                           date=datetime.now())
+
 
 @app.route("/news")
 def news():
@@ -22,12 +26,16 @@ def news():
         'tomorow': 'I walk to university',
 
     }
-    return render_template("news.html", news_dict=news_dict, sys_info=request.headers.get('User-Agent'), sys=sys.version, os_name=os.name, platform=platform.system(), release=platform.release(), date=datetime.now())
+    return render_template("news.html", news_dict=news_dict, sys_info=request.headers.get('User-Agent'),
+                           sys=sys.version, os_name=os.name, platform=platform.system(), release=platform.release(),
+                           date=datetime.now())
+
 
 @app.route("/")
 def portfolio():
-    return render_template("personal_portfolio.html", boolean=False, name='Nazar', error='Wrong data', sys_info=request.headers.get('User-Agent'), sys=sys.version, os_name=os.name, platform=platform.system(), release=platform.release(), date=datetime.now())
-
+    return render_template("personal_portfolio.html", boolean=False, name='Nazar', error='Wrong data',
+                           sys_info=request.headers.get('User-Agent'), sys=sys.version, os_name=os.name,
+                           platform=platform.system(), release=platform.release(), date=datetime.now())
 
 
 if __name__ == '__main__':
