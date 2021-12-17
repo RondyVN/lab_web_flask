9fba6c79fce1afdd79fbdd93df5815f53e21330d
RondyVN <rondelman15@gmail.com>
2021-12-17 15:55:59 +0200

    deploy7

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: gunicorn "run:create_app()"
+web: gunicorn "run:create_app(config_name='dev')"
 init: python commands.py create_tables
 init2: flask db init
--- config.py
+++ config.py
@@ -22,13 +22,13 @@ class DevConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
     WTF_CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1) or \
                               'sqlite:///' + os.path.join(basedir, 'site.db')
 
 
 class ProdConfig(Config):
     WTF_CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1) or \
                               'sqlite:///' + os.path.join(basedir, 'site.db')
 
 
